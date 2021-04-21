f06d58cf50091834003f6bdf53405a9b4436c811
Aishiya Ganesan <69270775+Healer-kid@users.noreply.github.com>
2021-04-21 18:47:06 +0530

    Update KthSmallestElement.cpp

--- KthSmallestElement.cpp
+++ KthSmallestElement.cpp
@@ -1,4 +1,5 @@
-Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array.
+It is given that all array elements are distinct.
 
 
 
