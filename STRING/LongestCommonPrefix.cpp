48351954cd97fa6e725536081051a3bf67c45e0f
Aishiya Ganesan <69270775+Healer-kid@users.noreply.github.com>
2021-05-28 12:35:03 +0530

    Add files via upload

---
+++ STRING/LongestCommonPrefix.cpp
@@ -0,0 +1,50 @@
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+
+
+LOGIC:
+
+We first sort the array of strings.
+Then, we choose the first and last string in the array. [They are supposed to be the most different among all the pairs of strings in the sorted array]
+We just compare how many common characters match from index i = 0 of these two strings.
+
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str) {
+        int n = str.size();
+        if(n==0) return "";
+        
+        string ans  = "";
+        sort(begin(str), end(str));
+        string a = str[0];
+        string b = str[n-1];
+        
+        for(int i=0; i<a.size(); i++){
+            if(a[i]==b[i]){
+                ans = ans + a[i];
+            }
+            else{
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
